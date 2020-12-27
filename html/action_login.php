b63750bd8d1f06d44e4b1aceded1724ccedab2ec
Héber Ribeiro <up201609026@fe.up.pt>
2020-12-27 21:03:04 +0000

    Added error messages in login and register actions

--- html/action_login.php
+++ html/action_login.php
@@ -8,7 +8,12 @@
   $email = $_POST['email'];
   $password = $_POST['password'];
 
-  if(!empty($email) && !empty($password)) {
+  if(empty($email) || empty($password)) {
+    $_SESSION['message'] = 'Deve preencher os dois campos!';
+    header("Location: templates/login.php");
+  }
+
+  else {
     $stmt = $dbh->prepare('SELECT Id FROM Conta WHERE Email = ? AND Password = ?');
     $stmt->execute(array($email, $password));
 
@@ -23,17 +28,20 @@
         $_SESSION['loggedin'] = true;
         $_SESSION['emailconta'] = $emailconta;
         $_SESSION['nomeconta'] = $nomeconta;
+
+        $_SESSION['message'] = 'Login bem sucedido. Bem vindo!';
         header("Location: homepage.php");
       }
 
       else {
+        $_SESSION['message'] = 'Ocorreu um erro! Tente de novo.';
         header("Location: templates/login.php");
       }
     }
 
     else {
+      $_SESSION['message'] = 'Email ou password errados! Tente de novo';
       header("Location: templates/login.php");
     }
-
   }
 ?>
--- html/action_register.php
+++ html/action_register.php
@@ -1,6 +1,10 @@
 <?php
   require_once('config/init.php');
 
+  if(!isset($_SESSION)) {
+    session_start();
+  }
+
   $email = $_POST['email'];
   $password = $_POST['password'];
   $nome = $_POST['nome'];
@@ -8,18 +12,50 @@
   $num_telefone = $_POST['num_telefone'];
   $nif = $_POST['nif'];
 
-  if(!empty($email) && !empty($password) && !empty($nome) && !empty($morada) && !empty($num_telefone) && !empty($nif)) {
+  if(empty($email) || empty($password) || empty($nome) || empty($morada) || empty($num_telefone) || empty($nif)) {
+    $_SESSION['message'] = 'Todos os campos devem ser preenchidos!';
+    header("Location: templates/register.php");
+  }
+
+  else {
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $_SESSION['message'] = 'Email introduzido inválido!';
+      header("Location: templates/register.php");
+      exit();
+    }
+
+    if(strlen($password) < 8) {
+      $_SESSION['message'] = 'A password deve ter pelo menos 8 carateres!';
+      header("Location: templates/register.php");
+      exit();
+    }
+
+    if(strlen((string)$num_telefone) != 9) {
+      $_SESSION['message'] = 'Número de telefone introduzido inválido!';
+      header("Location: templates/register.php");
+      exit();
+
+    }
+
+    if(strlen((string)$num_telefone) != 9) {
+      $_SESSION['message'] = 'NIF introduzido inválido!';
+      header("Location: templates/register.php");
+      exit();
+
+    }
+
     $stmt = $dbh->prepare('SELECT COUNT(*) FROM Conta WHERE Email = ?');
     $stmt->execute(array($email));
 
     if($stmt->fetchColumn()) {
+      $_SESSION['message'] = 'Já existe uma conta com este Email!';
       header("Location: templates/register.php");
+      exit();
     }
 
-    else {
-      $ca = $dbh->prepare('INSERT INTO Conta(Email, Password, Nome, Morada, Num_telefone, NIF) VALUES(? , ?, ?, ?, ?, ?)');
-      $ca->execute(array($email, $password, $nome, $morada, $num_telefone, $nif));
-      header("Location: templates/login.php");
-    }
+    $ca = $dbh->prepare('INSERT INTO Conta(Email, Password, Nome, Morada, Num_telefone, NIF) VALUES(? , ?, ?, ?, ?, ?)');
+    $ca->execute(array($email, $password, $nome, $morada, $num_telefone, $nif));
+    $_SESSION['message'] = 'Registo bem sucedido!';
+    header("Location: homepage.php");
   }
 ?>
