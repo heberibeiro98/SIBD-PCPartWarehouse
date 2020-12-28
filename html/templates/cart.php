9f2fd782d5c774e2b48e11e161492e9eb2130c24
Joao Gomes <7263846-CheSankara@users.noreply.gitlab.com>
2020-12-28 23:03:05 +0000

    merged commits

--- html/templates/cart.php
+++ html/templates/cart.php
@@ -1,37 +1,43 @@
-<section id="Cart">
-  <?php if(isset($_SESSION['cart'])) {?>
-    <h2>Carrinho</h2>
-      <table>
-        <tr>
-          <td></td>
-          <td>Produto</td>
-          <td>Preço</td>
-          <td>Quantidade</td>
-          <td>Remover</td>
-        </tr>
-        <?php $total = 0;
-          foreach ($_SESSION['cart'] as $Artigo) { ?>
+  <section id="Cart">
+    <?php if(isset($_SESSION['cart'])) {?>
+      <h3>Carrinho</h3>
+      <div class="orders">
+        <table>
           <tr>
-            <td><img src="/images/<?=$Artigo['Categoria']?>/<?=$Artigo['Modelo']?>.jpg"></td>
-            <td><?=$Artigo['Marca']?> <?=$Artigo['Modelo']?></td>
-            <td>€<?=number_format($Artigo['Preço'], 2)?></td>
-            <td><?=$Artigo['Quantidade']?></td>
-            <td><a href="/action_remove_from_cart.php?numserie=<?=$Artigo['Num_serie']?>"><i class="fa fa-times" style="color: red"></a></i></td>
-            <?php $total += ($Artigo['Preço']*$Artigo['Quantidade']); ?>
+            <td></td>
+            <td>Produto</td>
+            <td>Preço</td>
+            <td>Quantidade</td>
+            <td>Remover</td>
           </tr>
-        <?php } ?>
-      </table>
-  <h2>Total: €<?=number_format($total, 2)?></h2>
-  <form action="/action_checkout.php">
-    <input type="submit" value="Checkout">
-  </form>
-  <form action="/action_remove_from_cart.php" method="post">
-    <input type="hidden" name="limpar" value="limpar">
-    <input type="submit" value="Limpar carrinho">
-  </form>
-  <?php }
-  else { ?>
-  <h2>Carrinho vazio!</h2>
-<?php } ?>
-</section>
+          <?php $total = 0;
+            foreach ($_SESSION['cart'] as $Artigo) { ?>
+            <tr>
+              <td><img src="/images/<?=$Artigo['Categoria']?>/<?=$Artigo['Modelo']?>.jpg"></td>
+              <td><?=$Artigo['Marca']?> <?=$Artigo['Modelo']?></td>
+              <td>€<?=number_format($Artigo['Preço'], 2)?></td>
+              <td><?=$Artigo['Quantidade']?></td>
+              <td><a href="/action_remove_from_cart.php?numserie=<?=$Artigo['Num_serie']?>"><i class="fa fa-times" style="color: red"></a></i></td>
+              <?php $total += ($Artigo['Preço']*$Artigo['Quantidade']); ?>
+            </tr>
+          <?php } ?>
+        </table>
+      <div class="checkout">
+      <p>Total: €<?=number_format($total, 2)?></p>
+        <form action="/action_checkout.php">
+          <input type="submit" value="Checkout">
+        </form>
+      </div>
+      <div class="clean">
+        <form action="/action_remove_from_cart.php" method="post">
+          <input type="hidden" name="limpar" value="limpar">
+          <input type="submit" value="Limpar carrinho">
+        </form>
+      </div>
+      <?php }
+      else { ?>
+      <h2>O seu carrinho está vazio!</h2>
+    <?php } ?>
+    </div>
+  </section>
 </div>
--- html/templates/encomenda.php
+++ html/templates/encomenda.php
@@ -1,25 +1,28 @@
   <section id="Order">
-        <table>
+    <div class="orders">
+      <h3>Encomenda #<?=$numref?></h3>
+      <table>
+        <tr>
+          <td></td>
+          <td>Produto</td>
+          <td>Nº Série</td>
+          <td>Preço</td>
+          <td>Quantidade</td>
+        </tr>
+        <?php $total = 0;
+          foreach ($Artigos as $Artigo) { ?>
           <tr>
-            <td></td>
-            <td>Produto</td>
-            <td>Nº Série</td>
-            <td>Preço</td>
-            <td>Quantidade</td>
+            <td><img src="/images/<?=$Artigo['Categoria']?>/<?=$Artigo['Modelo']?>.jpg"></td>
+            <td><?=$Artigo['Marca']?> <?=$Artigo['Modelo']?></td>
+            <td>#<?=$Artigo['Num_serie']?></td>
+            <td>€<?=number_format($Artigo['Preço'], 2)?></td>
+            <?php $quantidade = GetProductQuantity($Artigo['Num_serie'], $numref); ?>
+            <td><?=$quantidade['Qtdd']?></td>
+            <?php $total += ($Artigo['Preço']*$quantidade['Qtdd']); ?>
           </tr>
-          <?php $total = 0;
-            foreach ($Artigos as $Artigo) { ?>
-            <tr>
-              <td><img src="/images/<?=$Artigo['Categoria']?>/<?=$Artigo['Modelo']?>.jpg"></td>
-              <td><?=$Artigo['Marca']?> <?=$Artigo['Modelo']?></td>
-              <td>#<?=$Artigo['Num_serie']?></td>
-              <td>€<?=number_format($Artigo['Preço'], 2)?></td>
-              <?php $quantidade = GetProductQuantity($Artigo['Num_serie'], $numref); ?>
-              <td><?=$quantidade['Qtdd']?></td>
-              <?php $total += ($Artigo['Preço']*$quantidade['Qtdd']); ?>
-            </tr>
-          <?php } ?>
-        </table>
-    <h2>Total: €<?=number_format($total, 2)?></h2>
+        <?php } ?>
+      </table>
+      <p>Total: €<?=number_format($total, 2)?></p>
+    </div>
   </section>
 </div>
