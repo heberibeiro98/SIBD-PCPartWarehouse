960ddca925620a234ba86f1bcd2c7c6ad0889fcc
Héber Ribeiro <up201609026@fe.up.pt>
2020-12-28 19:36:19 +0000

    Improved cart

---
+++ html/action_remove_from_cart.php
@@ -0,0 +1,26 @@
+<?php
+  require_once('config/init.php');
+  require_once('database/ProductList.php');
+
+  if(!isset($_SESSION)) {
+    session_start();
+  }
+
+  $limpar = $_POST['limpar'];
+  $numserie = $_GET['numserie'];
+
+  if(strcmp($limpar, 'limpar') == 0) {
+    unset($_SESSION['cart']);
+    header('Location: list_cart.php');
+    exit();
+  }
+
+  $_SESSION['cart'][$numserie]['Quantidade'] -= 1;
+
+  if($_SESSION['cart'][$numserie]['Quantidade'] == 0)
+    unset($_SESSION['cart'][$numserie]);
+
+
+  $_SESSION['message'] = 'Produto removido do carinho!';
+  header('Location: list_cart.php');
+?>
